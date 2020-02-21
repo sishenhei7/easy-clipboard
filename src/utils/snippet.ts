de75b7ec018b3fc7852fce560f33c4534958eede
sishenhei7 <982505136@qq.com>
2020-02-21 20:39:11 +0800

    feat: 完成demo

--- src/utils/snippet.ts
+++ src/utils/snippet.ts
@@ -1,9 +1,56 @@
-// prettier-ignore
 // hack: 代码里面不能有换行
-export const testCode =
+// prettier-ignore
+export const copyDirectlyCode =
 `// js
 import clipboard from 'easy-clipboard'
 clipboard.copy('This is some text.')
 `
 
-export const haha = `123`
+// prettier-ignore
+export const copyNodeCode =
+`// html
+<input id="js-copy-by-node" class="input" type="text" value="Copy by some Node." />
+
+// js
+import clipboard from 'easy-clipboard'
+const node = document.querySelector('#js-copy-by-node')
+clipboard.copy(node)
+`
+
+// prettier-ignore
+export const copyCallbackCode =
+`// html
+<input id="js-copy-by-callback" class="input" type="text" value="Copy by some callback." />
+
+// js
+import clipboard from 'easy-clipboard'
+clipboard.copy(() => document.querySelector('#js-copy-by-callback'))
+`
+
+// prettier-ignore
+export const copyTableCode =
+`// html
+<table id="js-copy-table" class="table">
+  <thead>
+    <tr>
+      <th>yesterday</th>
+      <th>today</th>
+      <th>tomorrow</th>
+    </tr>
+  </thead>
+  <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>5</td>
+    <td>6</td>
+  </tr>
+</table>
+
+// js
+import clipboard from 'easy-clipboard'
+clipboard.copy(() => document.querySelector('#js-copy-table'))
+`
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,53 +3,34 @@
     <h1>easy-clipboard.js</h1>
     <h2>Usage</h2>
     <h3>Copy string directly</h3>
-    <example-code :code="code" type="javascript">
+    <example-code :code="code1" type="javascript">
       <div class="flex justify-between lh-34">
         <span>Copy string directly.</span>
-        <button
-          class="button"
-          @click="handleCopyDirectly('This is some text.')"
-        >
+        <button class="button" @click="handleCopyDirectly('This is some text.')">
           Copy to clipboard
         </button>
       </div>
     </example-code>
     <h3>Copy by some Node</h3>
-    <example-code :code="code" type="html">
+    <example-code :code="code2" type="javascript">
       <div class="flex justify-between lh-34">
-        <input
-          id="js-copy-by-node"
-          class="input"
-          type="text"
-          value="Copy by some Node."
-        >
-        <button
-          class="button"
-          @click="handleCopyByNode('#js-copy-by-node')"
-        >
+        <input id="js-copy-by-node" class="input" type="text" value="Copy by some Node." />
+        <button class="button" @click="handleCopyByNode('#js-copy-by-node')">
           Copy to clipboard
         </button>
       </div>
     </example-code>
     <h3>Copy by some callback</h3>
-    <example-code :code="code" type="html">
+    <example-code :code="code3" type="javascript">
       <div class="flex justify-between lh-34">
-        <input
-          id="js-copy-by-callback"
-          class="input"
-          type="text"
-          value="Copy by some callback."
-        >
-        <button
-          class="button"
-          @click="handleCopyByNode('#js-copy-by-callback')"
-        >
+        <input id="js-copy-by-callback" class="input" type="text" value="Copy by some callback." />
+        <button class="button" @click="handleCopyByNode('#js-copy-by-callback')">
           Copy to clipboard
         </button>
       </div>
     </example-code>
     <h3>Copy a whole table(to a excel)</h3>
-    <example-code :code="code" type="html">
+    <example-code :code="code4" type="html">
       <div class="flex justify-between">
         <table id="js-copy-table" class="table">
           <thead>
@@ -70,11 +51,9 @@
             <td>6</td>
           </tr>
         </table>
+
         <div>
-          <button
-            class="button"
-            @click="handleCopyByNode('#js-copy-table')"
-          >
+          <button class="button" @click="handleCopyByNode('#js-copy-table')">
             Copy to clipboard
           </button>
         </div>
@@ -87,7 +66,7 @@
 import { Component, Vue } from 'vue-property-decorator'
 import clipboard from 'easy-clipboard'
 import ExampleCode from '@/components/ExampleCode.vue'
-import { testCode } from '@/utils/snippet'
+import { copyDirectlyCode, copyNodeCode, copyCallbackCode, copyTableCode } from '@/utils/snippet'
 
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 @Component({
@@ -96,19 +75,22 @@ import { testCode } from '@/utils/snippet'
   }
 })
 export default class Home extends Vue {
-  private code: string = testCode
+  private code1: string = copyDirectlyCode
+  private code2: string = copyNodeCode
+  private code3: string = copyCallbackCode
+  private code4: string = copyTableCode
 
   handleCopyDirectly(str: string) {
     clipboard.copy(str)
   }
 
   handleCopyByNode(selector: string) {
-    const node = document.querySelector(selector);
-    clipboard.copy(node);
+    const node = document.querySelector(selector)
+    clipboard.copy(node)
   }
 
   handleCopyByCallback(selector: string) {
-    clipboard.copy(() => document.querySelector(selector));
+    clipboard.copy(() => document.querySelector(selector))
   }
 }
 </script>
