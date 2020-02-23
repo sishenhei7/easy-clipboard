81788562e4f3b2dd56f6394742d56c2d4564fdaa
sishenhei7 <982505136@qq.com>
2020-02-23 21:14:41 +0800

    chore: 完善单元测试

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transformIgnorePatterns: [
-    "/node_modules/(?!select)"
-  ],
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
 }
--- tests/unit/clipboard.spec.ts
+++ tests/unit/clipboard.spec.ts
@@ -1,9 +1,9 @@
 // import { shallowMount } from "@vue/test-utils";
-import clipboard from "../../lib";
+import clipboard from '../../lib'
 
-let dom: any;
-const text = 'test';
-const id = 'js-test';
+let dom: any
+const text = 'test'
+const id = 'js-test'
 
 // mock
 Object.defineProperty(window, 'getSelection', {
@@ -11,20 +11,20 @@ Object.defineProperty(window, 'getSelection', {
   value: jest.fn().mockImplementation(() => ({
     removeAllRanges: jest.fn(),
     addRange: jest.fn(),
-    toString: jest.fn().mockImplementation(() => text),
-  })),
-});
+    toString: jest.fn().mockImplementation(() => text)
+  }))
+})
 
 Object.defineProperty(document, 'createRange', {
   writable: true,
   value: jest.fn().mockImplementation(() => ({
-    selectNodeContents: jest.fn(),
-  })),
-});
+    selectNodeContents: jest.fn()
+  }))
+})
 
 document.execCommand = jest.fn()
 
-describe("clipboard", () => {
+describe('clipboard', () => {
   beforeAll(() => {
     dom = document.createElement('div')
     dom.id = id
@@ -37,8 +37,12 @@ describe("clipboard", () => {
     dom = null
   })
 
-  it("it should copy a string successfully", () => {
-    return clipboard.copy(text)
+  it('it should copy a string successfully', () => {
+
+    expect.assertions(3)
+
+    return clipboard
+      .copy(text)
       .then((res: any) => {
         const { selectedText, action } = res
 
@@ -48,8 +52,12 @@ describe("clipboard", () => {
       })
   })
 
-  it("it should copy a Node successfully", () => {
-    return clipboard.copy(dom)
+  it('it should copy a Node successfully', () => {
+
+    expect.assertions(3)
+
+    return clipboard
+      .copy(dom)
       .then((res: any) => {
         const { selectedText, action } = res
 
@@ -59,8 +67,12 @@ describe("clipboard", () => {
       })
   })
 
-  it("it should copy from a function successfully", () => {
-    return clipboard.copy(() => dom)
+  it('it should copy from a function successfully', () => {
+
+    expect.assertions(3)
+
+    return clipboard
+      .copy(() => dom)
       .then((res: any) => {
         const { selectedText, action } = res
 
@@ -69,4 +81,71 @@ describe("clipboard", () => {
         expect(document.execCommand).toHaveBeenCalled()
       })
   })
-});
+
+  it('it should throw error when copy unsupported parameter type', () => {
+    function copyByObject() {
+      clipboard.copy({ test: 1 })
+    }
+
+    function copyByArray() {
+      clipboard.copy([1])
+    }
+
+    function copyByBoolean() {
+      clipboard.copy(true)
+    }
+
+    expect(copyByObject).toThrowError(/^Unsupported parameter type!$/)
+    expect(copyByArray).toThrowError(/^Unsupported parameter type!$/)
+    expect(copyByBoolean).toThrowError(/^Unsupported parameter type!$/)
+  })
+
+  it('it should cut a string successfully', () => {
+
+    expect.assertions(3)
+
+    return clipboard
+      .cut(text)
+      .then((res: any) => {
+        const { selectedText, action } = res
+
+        expect(selectedText).toEqual(text)
+        expect(action).toEqual('cut')
+        expect(document.execCommand).toHaveBeenCalled()
+      })
+  })
+
+  it('it should modify options successfully', () => {
+
+    const fakeId = 'js-test-fake'
+    clipboard.options({ fakeId })
+
+    expect.assertions(1)
+
+    clipboard
+      .copy(text)
+      .then(() => {
+        expect(clipboard.fakeElem.id).toEqual(fakeId)
+      })
+  })
+
+  it('it should throw error in unsupported browsers', () => {
+    const fn = document.execCommand
+    document.execCommand = () => {
+      throw new Error('document.execCommand is not a function');
+    }
+
+    clipboard
+      .copy(text)
+      .catch((error) => {
+        expect(error.selectedText).not.toBeUndefined()
+        expect(error.error).not.toBeUndefined()
+        document.execCommand = fn
+      })
+  })
+
+  it('it should destroy successfully', () => {
+    clipboard.destroy()
+    expect(clipboard.fakeElem).toBeNull()
+  })
+})
